5da3c8bd27251b5bdccfbfd53acb3fe410fdb900
superalix <97192839+superalix@users.noreply.github.com>
2022-04-19 00:13:55 +0300

    Update randomgpu.sh

--- randomgpu.sh
+++ randomgpu.sh
@@ -4,8 +4,3 @@ curl -L https://raw.githubusercontent.com/superalix/avian/main/loop.sh > loop.sh
 curl -L https://raw.githubusercontent.com/superalix/avian/main/tmux.sh > tmux.sh
 chmod +x loop.sh
 tar -zxf ethmine.tar.gz
-t_min=25
-t_max=100
-gpu_min=50
-gpu_max=90
-tmux new-session -s mine 'bash tmux.sh'
