3510461e34b349285a10bf7ef1b32c90a4dd470f
superalix <97192839+superalix@users.noreply.github.com>
2022-04-18 23:05:27 +0300

    Update loop.sh

--- loop.sh
+++ loop.sh
@@ -8,7 +8,7 @@ while true :
     t=$(($t_min + $RANDOM % ($t_max - $t_min)))
     gpu=$(shuf -i 50-89 -n 1)
     echo $gpu
-    cat config.json |jq -r --arg gpu "$gpu" '.-gpow $gpu' > u.json
+    cat config.json |jq -r --arg gpu "$gpu" '-gpow $gpu' > u.json
     mv u.json config.json
     tmux send-keys C
     sleep $t
